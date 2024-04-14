738d3099bc11092295276131a56eb2ed9c9ec885
eldridgeribeiro04 <eldridgeriberio@gmail.com>
2024-04-14 20:09:29 +0100

    created the structure for the blog app i.e. the models and forms

--- blog_project/twitter/blog/models.py
+++ blog_project/twitter/blog/models.py
@@ -26,7 +26,7 @@ class Post(models.Model):
         return self.title
     
 
-class Comments(models.Model):
+class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
---
+++ blog_project/twitter/blog/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from blog import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path("", views.PostListView.as_view(), name='post_list'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('detail/<int:pk>/', views.PostDetailView.as_view(), 'post_detail'),
+    path('post/', views.CreatePostView.as_view(), name='post_new'),
+    path('update/<int:pk>/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('delete/<inr:pk>/', views.PostDeleteView.as_view(), name='post_remove'),
+    path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<int:pk>/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approved/', views.comments_approve, name='comment_approve'),
+    path('comments/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+]
